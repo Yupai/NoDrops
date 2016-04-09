9bae55a2c9e2bc086e329dfcb71a79c593b760ea
Yupai <pvpyupai@hotmail.com>
2016-04-08 19:18:56 -0500

    Add files

---
+++ src/yupai/NoDrops/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace yupai\NoDrops;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDropItemEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener {
+
+    public function OnEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this ,$this);
+        $this->getLogger()->info(TextFormat::GREEN . "NoDrops by Yupai Enable!");
+    }
+
+    public function OnDisable(){
+        $this->getLogger()->info(TextFormat::RED . "NoDrops by Yupai Disable!");
+    }
+
+    public function onDrop(PlayerDropItemEvent $event){
+        $player = $event->getPlayer();
+        $event->setCancelled(true);
+    } 
+}
